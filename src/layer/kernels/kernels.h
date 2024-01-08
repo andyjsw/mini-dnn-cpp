f9daae51d653ea5eac027692da38ca3bd4443cf1
andyjsw <91446731+andyjsw@users.noreply.github.com>
2024-01-08 22:27:43 +0700

    Changes to be committed: 	modified:   .vscode/settings.json 	new file:   src/layer/convGPU2.cc 	new file:   src/layer/convGPU2.h 	modified:   src/layer/kernels/kernels.h

--- src/layer/kernels/kernels.h
+++ src/layer/kernels/kernels.h
@@ -12,7 +12,8 @@ public:
     void get_device_info();
     void conv_forward(const float* in, float* out, const float* weight, const int n_samples, 
         const int channel_in, const int channel_out, 
-        const int height_in, const int width_in, const int kernel_width);
+        const int height_in, const int width_in, const int kernel_width,
+        const int kernel_type);
 };
 
 #endif // SRC_LAYER_KERNELS_KERNELS_H_
